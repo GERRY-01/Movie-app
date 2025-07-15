70115dce016c57de99236ebe3f9da319c81d9fcc
Gerry-01 <odhiambogerry010@gmail.com>
2025-07-15 17:20:11 +0300

    added the allowed hosts

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -27,8 +27,7 @@ API_KEY = config('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
 # Application definition
 
